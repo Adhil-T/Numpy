561b5c53bfbecbf22c6b67379b8b37e5cf0738b3
Adhil T <139242219+Adhil-T@users.noreply.github.com>
2026-02-23 11:22:20 +0530

    Add script to calculate mean, median, and std deviation

---
+++ mean median standard deviation.py
@@ -0,0 +1,11 @@
+import numpy
+
+arr = numpy.array([10,20,30, 40,50])
+
+mean = numpy.mean(arr)
+median = numpy.median(arr)
+std_dev = numpy.std(arr)
+
+print("Mean: ", mean)
+print("Median: ", median)
+print("Standard Deviation: ", std_dev)
